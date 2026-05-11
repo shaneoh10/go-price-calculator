1feb0d1f50b7098f2274d8ab5723958b83a349dc
Shane O'Hanlon <70384009+shaneoh10@users.noreply.github.com>
2026-05-11 15:42:11 +0900

    Set up tax included price calculator job

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"github.com/shaneoh10/go-price-calculator/prices"
+)
 
+func main() {
+	taxRates := []float64{0, 0.09, 0.12, 0.21}
+
+	for _, taxRate := range taxRates {
+		job := prices.NewTaxIncludedPriceJob(taxRate)
+		job.Process()
+	}
 }
